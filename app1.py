0134e0d36f27f349613d1bdebb913065a8b2eea7
Anu929 <141702224+Anu929@users.noreply.github.com>
2026-03-19 18:52:02 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
